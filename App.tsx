1b2cf3b0410ba6a800b69f7934f87359be4c387d
brokenginger <98916356+brokenginger@users.noreply.github.com>
2025-09-16 15:37:57 -0700

    Update and rename App.tsx to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Header } from './components/Header';
+import Header from './components/Header';
 import { Footer } from './components/Footer';
 import { Card } from './components/Card';
 import { Accordion } from './components/Accordion';
